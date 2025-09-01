3ff7f43e94c530c7dfe84cc16edd42fea97caee4
Bojan Kuljić <bkuljic@gmail.com>
2025-09-01 20:57:59 +0200

    vars_02.py
    vars_02.py

---
+++ vars_02.py
@@ -0,0 +1,27 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovaraujuću vrijednost) za:
+- Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
+- Stranice a i b, četverokuta te za površinu tog četverokuta
+'''
+ime = 'Bojan'
+prezime = 'Kuljić'
+godina_rodjenja = 1987
+drzava = "Hrvatska"
+Zaposlen = True
+tezina = 82.3
+spol = 'M'
+
+print("Ime",  ime, "Prezime", prezime);
+
+a = 5
+b = 2
+povrsina = a * b
+print("Površina četverokuta: ", a*b)
+
+name = 'Bojan'
+surname = 'Kuljić'
+date_of_birth = 1987
+country = "Hrvatska"
+Employed = True
+weight = 82.3
+sex = 'M'
