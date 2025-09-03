c44e174da6b75c8def6403724a19364e42ad9a5b
Bojan Kuljić <bkuljic@gmail.com>
2025-09-03 19:21:12 +0200

    Create vars_03.py

---
+++ vars_03.py
@@ -0,0 +1,55 @@
+# Ovo je komentar
+
+'''
+Potrebno je kreirati varijable u kojima će biti pohranjene
+proizvoljne dužine trokuta.
+'''
+
+a = 5
+b = 6
+c = 7
+
+'''
+Potrebno je kreirati varijable u kojima će biti pohranjeni podaci o
+filmu: naziv filma, godina proizvodnje, proječna ocjena, kratki
+opis
+'''
+
+movie = 'The Matrix'
+release_year = 1999
+svg_rate = 9.99
+summary = 'Lorem ipsum dolor sit ...'
+
+'''
+Potrebno je kreirati varijable u kojima će biti pohranjeni podaci o
+imenu i prezimenu studenta, završnoj ocjeni te podataka je li
+diplomirao ili nije
+'''
+
+first_name = 'Pero'
+last_name = 'Perić'
+final_score = 4.98
+is_graduated = True
+
+'''
+Kreirati varijablu za čuvanje podataka o studentu: ime i prezime,
+OIB, email, broj telefona, prosječna ocjena, godina upisa
+studija, naziv studija, naziv sveučilišta
+'''
+
+# Opcija 1
+student = 'Pero Peric, 12345678901; pero.peric@gmail.com'
+
+# Opcija 2
+
+first_name = 'Pero'
+last_name = 'Peric'
+oib = '12345678901',
+email = 'pero.peric@example.com',
+telefon =  '+385 91 123 4567',
+prosjek = 4.31,
+godina_upisa = 2022,
+studij = 'Računarstvo',
+sveuciliste = 'Sveučilište u Zagrebu'
+
+
